7c2a3b8910913c7a252437f6c6f5c2b562d452d5
alunos <alunos@IFRSL.redelocal>
2024-06-05 10:51:52 -0300

    inicializacao do loop e proporcoes da tela

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,19 @@ int main()
 {
     Color darkGreen = Color{20, 160, 133, 255};
 
-    const int screenWidth = 800;
+    const int screenWidth = 300;
     const int screenHeight = 600;
 
-    InitWindow(screenWidth, screenHeight, "My first RAYLIB program!");
+    InitWindow(screenWidth, screenHeight, "Tetris com Raylib");
     SetTargetFPS(60);
 
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(darkGreen);
+        EndDrawing();
+    }
+
+    CloseWindow();
     return 0;
 }
